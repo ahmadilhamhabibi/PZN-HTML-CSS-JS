7605f1fa8a9f374d50f13f8e2bb38221b8ce9cbf
habibi-dev <ahmadilhamhabibi@gmail.com>
2025-07-15 15:02:05 +0700

    belajar-javascript-modules/

---
+++ belajar-javascript-modules/scripts/object-module.js
@@ -0,0 +1,14 @@
+export const firstName = "Ahmad";
+export const middleName = "Ilham";
+export const lastName = "Habibi";
+
+
+export function hello() {
+    console.info(`Hello from diriku`);
+}
+
+export class Person {
+    constructor() {
+        this.name = "Habibi";
+    }
+}
